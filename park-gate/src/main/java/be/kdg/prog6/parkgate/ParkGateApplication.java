80e2a22a3c43bec21222a632c33bd08305e2a2db
noah.diderich <noah.diderich@student.kdg.be>
2024-06-17 16:14:14 +0200

    create park gate module #9

---
+++ park-gate/src/main/java/be/kdg/prog6/parkgate/ParkGateApplication.java
@@ -0,0 +1,13 @@
+package be.kdg.prog6.parkgate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkGateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkGateApplication.class, args);
+    }
+
+}
