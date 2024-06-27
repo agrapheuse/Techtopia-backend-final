52a3493076669fdfbcd1f4e650a55bbcbd015270
noah.diderich <noah.diderich@student.kdg.be>
2024-06-27 15:25:01 +0200

    create ticket module #13

---
+++ visitor-information-system/src/main/java/be/kdg/prog6/visitorInformationSystem/adapters/in/web/PointOfInterestController.java
@@ -0,0 +1,10 @@
+package be.kdg.prog6.visitorInformationSystem.adapters.in.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pointOfInterest")
+public class PointOfInterestController {
+
+}
