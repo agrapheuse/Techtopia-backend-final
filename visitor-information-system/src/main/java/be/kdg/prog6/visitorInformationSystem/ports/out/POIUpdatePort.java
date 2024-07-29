fbbf2f70d98cd41de0658ff64bce9249ca40c029
noah.diderich <noah.diderich@student.kdg.be>
2024-07-29 16:51:10 +0200

    #45

--- visitor-information-system/src/main/java/be/kdg/prog6/visitorInformationSystem/ports/out/POIUpdatePort.java
+++
@@ -1,7 +0,0 @@
-package be.kdg.prog6.visitorInformationSystem.ports.out;
-
-import be.kdg.prog6.visitorInformationSystem.domain.PointOfInterest;
-
-public interface POIUpdatePort {
-    void updatePOI(PointOfInterest pointOfInterest);
-}
