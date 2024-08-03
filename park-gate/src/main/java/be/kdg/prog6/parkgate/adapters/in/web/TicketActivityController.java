f4d7378021106b9847b959c74646fe5f0306ee1e
noah.diderich <noah.diderich@student.kdg.be>
2024-08-03 13:37:54 +0200

    exit park

--- park-gate/src/main/java/be/kdg/prog6/parkgate/adapters/in/web/TicketActivityController.java
+++ park-gate/src/main/java/be/kdg/prog6/parkgate/adapters/in/web/TicketActivityController.java
@@ -1,6 +1,7 @@
 package be.kdg.prog6.parkgate.adapters.in.web;
 
 import be.kdg.prog6.parkgate.ports.in.EnterParkUseCase;
+import be.kdg.prog6.parkgate.ports.in.ExitParkUseCase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -16,10 +17,12 @@ import java.util.UUID;
 @RequestMapping("/ticketActivity")
 public class TicketActivityController {
     private final EnterParkUseCase enterParkUseCase;
+    private final ExitParkUseCase exitParkUseCase;
     public static final Logger log = LoggerFactory.getLogger(TicketController.class);
 
-    public TicketActivityController(EnterParkUseCase enterParkUseCase) {
+    public TicketActivityController(EnterParkUseCase enterParkUseCase, ExitParkUseCase exitParkUseCase) {
         this.enterParkUseCase = enterParkUseCase;
+        this.exitParkUseCase = exitParkUseCase;
     }
 
     @PostMapping("/enter")
@@ -35,19 +38,12 @@ public class TicketActivityController {
         }
     }
 
-    //TODO: add exitPark && visitPOI
-    /*
     @PostMapping("/exit")
     public ResponseEntity<Void> exitPark(@RequestParam String ticketUUID) {
-        log.debug("exiting park with ticket UUID: {}", ticketUUID);
+        log.debug("Exiting park with ticket UUID: {}", ticketUUID);
 
         try {
-            createTicketActivityUseCase.createTicketActivity(new TicketActivity(
-                    UUID.fromString(ticketUUID),
-                    null,
-                    ActivityType.EXITED,
-                    LocalDateTime.now()
-            ));
+            exitParkUseCase.exitPark(UUID.fromString(ticketUUID));
             return new ResponseEntity<>(HttpStatus.CREATED);
         } catch (Exception e) {
             log.error("Error creating ticket activity", e);
@@ -55,6 +51,9 @@ public class TicketActivityController {
         }
     }
 
+    //TODO: add visitPOI
+
+/*
     @PostMapping("/visitedPOI")
     public ResponseEntity<Void> visitPOI(
             @RequestParam String ticketUUID,
---
+++ park-gate/src/main/java/be/kdg/prog6/parkgate/core/DefaultExitParkUseCase.java
@@ -0,0 +1,42 @@
+package be.kdg.prog6.parkgate.core;
+
+import be.kdg.prog6.parkgate.domain.ActivityType;
+import be.kdg.prog6.parkgate.domain.Status;
+import be.kdg.prog6.parkgate.domain.Ticket;
+import be.kdg.prog6.parkgate.domain.TicketActivity;
+import be.kdg.prog6.parkgate.ports.in.ExitParkUseCase;
+import be.kdg.prog6.parkgate.ports.out.TicketActivityCreatedPort;
+import be.kdg.prog6.parkgate.ports.out.TicketLoadedPort;
+import be.kdg.prog6.parkgate.ports.out.TicketUpdatedPort;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+public class DefaultExitParkUseCase implements ExitParkUseCase {
+    private final TicketLoadedPort ticketLoadedPort;
+    private final TicketUpdatedPort ticketUpdatedPort;
+    private final TicketActivityCreatedPort ticketActivityCreatedPort;
+
+    public DefaultExitParkUseCase(TicketLoadedPort ticketLoadedPort, TicketUpdatedPort ticketUpdatedPort, TicketActivityCreatedPort ticketActivityCreatedPort) {
+        this.ticketLoadedPort = ticketLoadedPort;
+        this.ticketUpdatedPort = ticketUpdatedPort;
+        this.ticketActivityCreatedPort = ticketActivityCreatedPort;
+    }
+
+    @Override
+    public void exitPark(UUID ticketUUID) {
+        Ticket ticket = ticketLoadedPort.getTicket(ticketUUID);
+        ticket.setStatus(Status.EXITED);
+        ticketUpdatedPort.updateTicket(ticket);
+        ticketActivityCreatedPort.createTicketActivity(
+                new TicketActivity(
+                        ticket.getUuid().uuid(),
+                        null,
+                        ActivityType.EXITED,
+                        LocalDateTime.now()
+                )
+        );
+    }
+}
